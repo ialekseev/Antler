d851d4d42b97c0ce9c1232a31f962c057cc405a8
Igor Alekseev <alekseevi15@gmail.com>
2013-12-02 19:08:08 +0400

    New solution for Core projects; Updated NuGet.exe; Script for creating NuGet package for Core project(first try)

---
+++ build/core/build-core.cmd
@@ -0,0 +1,26 @@
+@echo off
+set path=%path%;C:/Windows/Microsoft.NET/Framework/v4.0.30319;
+
+::echo Under construction
+::goto end
+
+echo Building project...
+msbuild ../../src/Antler.Core.sln /nologo /v:q /p:Configuration=Release /t:Clean
+msbuild ../../src/Antler.Core.sln /nologo /v:q /p:Configuration=Release /clp:ErrorsOnly
+
+echo Merging assemblies...
+if exist output rmdir /s /q output
+mkdir output
+mkdir output\lib
+mkdir output\lib\net40
+
+..\..\tools\ilmerge\ILMerge.exe /keyfile:..\..\src\Antler.snk /target:library ^
+ /targetplatform:"v4,C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319" ^
+ /out:"output\lib\net40\Antler.Core.dll" ^
+ "..\..\src\main\Antler-Abstractions\bin\Release\SmartElk.Antler.Abstractions.dll" ^
+ "..\..\src\main\Antler-Common\bin\Release\SmartElk.Antler.Common.dll" ^
+ "..\..\src\main\Antler-Domain\bin\Release\SmartElk.Antler.Domain.dll" 
+
+echo Done.
+
+:end
--- src/main/Antler-Domain/Properties/AssemblyInfo.cs
+++ src/main/Antler-Domain/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Antler-Domain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Antler-Core")]
+[assembly: AssemblyDescription("Antler-Core")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SmartElk")]
-[assembly: AssemblyProduct("Antler-Domain")]
+[assembly: AssemblyProduct("Antler-Core")]
 [assembly: AssemblyCopyright("SmartElk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+
