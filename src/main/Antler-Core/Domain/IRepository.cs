d51985fc010f8a202b8ccac687e420b4892aea16
Igor Alekseev <alekseevi15@gmail.com>
2014-03-18 18:43:14 +0400

    Adding support for updating entities explicitly (first try)

--- src/main/Antler-Core/Domain/IRepository.cs
+++ src/main/Antler-Core/Domain/IRepository.cs
@@ -7,6 +7,7 @@ namespace SmartElk.Antler.Core.Domain
         IQueryable<TEntity> AsQueryable();
         TEntity GetById<TId>(TId id);        
         void Insert(TEntity entity);
+        void Update(TEntity entity); //todo: cover by integration tests
         void Delete(TEntity entity);
         void Delete<TId>(TId id);
     }
