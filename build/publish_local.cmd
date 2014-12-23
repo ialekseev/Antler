d1d39f28884fa919caac98ea38468ed1ece58aaa
Igor Alekseev <alekseevi15@gmail.com>
2014-12-23 15:01:54 +0300

    Updating packages + Sample project

--- build/publish_local.cmd
+++ build/publish_local.cmd
@@ -1,12 +1,12 @@
 @echo off
 set path=%path%;C:/Windows/Microsoft.NET/Framework/v4.0.30319;
-set version=3.2.0-alpha
+set version=3.3.0
 set skipBuild=false
 set skipTests=false
 set skipCopyingBuiltAssemblies=false
 set skipDependentPackagesVersionsUpdate=false
 set skipCreatingNuGetPackages=false
-set skipPublishing=true
+set skipPublishing=false
 
 call publish_common.cmd
 
